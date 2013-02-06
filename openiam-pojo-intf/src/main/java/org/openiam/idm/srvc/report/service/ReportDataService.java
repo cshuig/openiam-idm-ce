c8bc023f3127ee40cae253044d84ba4cdb29187f
Ekta Agarwal <ekta.agarwal@openiam.com>
2013-02-06 22:01:16 +0530

    for reports after merge, deletion - 1st cut

--- openiam-pojo-intf/src/main/java/org/openiam/idm/srvc/report/service/ReportDataService.java
+++ openiam-pojo-intf/src/main/java/org/openiam/idm/srvc/report/service/ReportDataService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.openiam.idm.srvc.report.domain.ReportCriteriaParamEntity;
 import org.openiam.idm.srvc.report.domain.ReportInfoEntity;
+import org.openiam.idm.srvc.report.domain.ReportSubscriptionEntity;
 import org.openiam.exception.ScriptEngineException;
 import org.openiam.idm.srvc.report.domain.ReportParamTypeEntity;
 import org.openiam.idm.srvc.report.dto.ReportCriteriaParamDto;
@@ -19,8 +20,10 @@ import org.openiam.idm.srvc.report.dto.ReportTable;
 public interface ReportDataService {
     ReportDataDto getReportData(final String reportName, final Map<String, String> reportParams) throws ClassNotFoundException, ScriptEngineException, IOException;
     List<ReportInfoEntity> getAllReports();
+    List<ReportSubscriptionEntity> getAllSubscribedReports();
     ReportInfoEntity getReportByName(String name);
     void createOrUpdateReportInfo(String reportName, String reportDataSource, String reportUrl);
+    void createOrUpdateSubscribedReportInfo(String reportName, String reportDataSource, String reportUrl);
     List<ReportCriteriaParamEntity> getReportParametersByReportId(String reportId);
     void updateReportParametersByReportName(String reportName, List<ReportCriteriaParamEntity> prameters);
     List<ReportParamTypeEntity> getReportParameterTypes();
