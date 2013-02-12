61fed745d467d8c7f19a38c11c69bd18cd7aa64a
Ekta Agarwal <ekta.agarwal@openiam.com>
2013-02-12 23:55:41 +0530

    Completed cron job

--- idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/batch/birt/ReportGenerator.java
+++ idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/batch/birt/ReportGenerator.java
@@ -1,24 +1,35 @@
 package org.openiam.idm.srvc.batch.birt;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.core.framework.Platform;
-import org.eclipse.birt.report.engine.api.*;
+import org.eclipse.birt.report.engine.api.EngineConfig;
+import org.eclipse.birt.report.engine.api.EngineException;
+import org.eclipse.birt.report.engine.api.HTMLRenderOption;
+import org.eclipse.birt.report.engine.api.IReportEngine;
+import org.eclipse.birt.report.engine.api.IReportEngineFactory;
+import org.eclipse.birt.report.engine.api.IReportRunnable;
+import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
+import org.eclipse.birt.report.engine.api.PDFRenderOption;
+//import org.eclipse.birt.report.engine.api.*;
 import org.eclipse.core.internal.registry.RegistryProviderFactory;
-import org.openiam.idm.srvc.batch.NightlyTask;
+import org.openiam.idm.srvc.msg.service.MailService;
 
 
 public class ReportGenerator {
 	private static ResourceBundle res = ResourceBundle.getBundle("securityconf");
 	private static final Log log = LogFactory.getLog(ReportGenerator.class);
 
-	public static void generateReport(String reportName, String reportDesign, String format, String userId, Map params) throws BirtException, EngineException {
+	public static void generateReport(String reportName, String reportDesign, String format, String userId, Map params, String deliveryMethod, List<String> emailAddresses, List<String> userIds, MailService mailService) throws BirtException, EngineException {
 		EngineConfig config = new EngineConfig( );
 		Platform.startup( config );  //If using RE API in Eclipse/RCP application this is not needed.
 		IReportEngineFactory factory = (IReportEngineFactory) Platform
@@ -39,10 +50,13 @@ public class ReportGenerator {
 		
 		task.validateParameters();
 
+		String outputDir = res.getString("reportRoot") + "//GENERATED_REPORTS//" + deliveryMethod + "//" ;
+		String outputFileName = outputDir + userId + "//" + reportName;
 		if ("HTML".equalsIgnoreCase(format)){
 			//Setup rendering to HTML
 			HTMLRenderOption options = new HTMLRenderOption();
-			options.setOutputFileName(res.getString("reportRoot") + "//GENERATED_REPORTS//" + userId + "//" +reportName + ".html");
+			outputFileName += ".html";
+			options.setOutputFileName(outputFileName);
 			options.setOutputFormat(format); // HTML or PDF
 			//Setting this to true removes html and body tags
 			options.setEmbeddable(false);
@@ -50,8 +64,8 @@ public class ReportGenerator {
 		}else{
 			//Setup rendering to HTML
 			PDFRenderOption options = new PDFRenderOption();
-			//TODO provide userid in output file path
-			options.setOutputFileName(res.getString("reportRoot") + "//GENERATED_REPORTS//" + userId + "//" +reportName + ".html");
+			outputFileName += ".pdf";
+			options.setOutputFileName(outputFileName);
 			options.setOutputFormat(format); // HTML or PDF
 			//Setting this to true removes html and body tags
 			task.setRenderOption(options);
@@ -63,6 +77,25 @@ public class ReportGenerator {
 		Platform.shutdown();
 		//Bugzilla 351052
 		RegistryProviderFactory.releaseDefault();
+		
+		//Email the reports or replicate across userids as applicable
+		if ("EMAIL".equalsIgnoreCase(deliveryMethod)){
+			//email reports
+			//TODO check fromAddress
+			mailService.send("BIRTReportSender", (String[])emailAddresses.toArray(), "Subscribed Report", "Please find your subscribed report attached.", false, outputFileName);
+		}else{
+			//replicate across userIds
+			File file = new File(outputFileName);
+			for (String userID : userIds){
+				File destDir = new File(outputDir + "//" + userID);
+				try {
+					FileUtils.copyFileToDirectory(file, destDir);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	public static void deleteGeneratedDirs() {
