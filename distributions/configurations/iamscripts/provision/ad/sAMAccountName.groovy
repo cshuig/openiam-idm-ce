ef190950ccf1287cbba8c3229d7837a0a5e55f38
Vitaly Yakunin <vitaly.yakunin@gmail.com>
2013-04-09 14:54:38 +0300

    revert default samAccountName script

--- distributions/configurations/iamscripts/provision/ad/sAMAccountName.groovy
+++ distributions/configurations/iamscripts/provision/ad/sAMAccountName.groovy
@@ -1,13 +1,40 @@
-import org.openiam.idm.srvc.user.dto.UserAttribute
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
-if(org.apache.commons.lang.StringUtils.isNotEmpty(targetSystemOperation.toString()) && org.openiam.base.AttributeOperationEnum.REPLACE.equals(targetSystemOperation)) {
-    output = targetSystemIdentity;
-} else {
-    UserAttribute attr = user.getUserAttributes().get("samAccountName");
-	if (attr != null && attr.value != null ) {
-		output =  attr.value;
-	} else {
-		output=user.employeeId;
-	}
+def loginManager = context.getBean("loginManager")
+
+// DEFAULT SECURITY DOMAIN
+def secDomain = "USR_SEC_DOMAIN";
+
+ctr = 1;
+
+loginID=lg.id.login;
+
+if (securityDomain != null) {
+    secDomain = securityDomain;
 }
+
+
+if (loginID.length() > 17) {
+    loginID = loginID.substring(0,17);
+
+    // add logic to ensure uniqueness
+
+    if (managedSysId != null) {
+
+        origLoginID = loginId;
+
+        while ( loginManager.loginExists( secDomain, loginID, managedSysId )) {
+            strCtrSize = String.valueOf(ctr)
+            loginId=   origLoginID + ctr;
+            ctr++
+        }
+
+    }
+
+
+}
+
+
+output=loginID
+
 
