a1ea0177f68fff6f8e955160a307f27c1feb5dac
Vitaly Yakunin <vitaly.yakunin@gmail.com>
2013-05-03 17:07:57 +0300

    improved pattern for AD by role

--- distributions/configurations/iamscripts/recon/ADSearchQuery.groovy
+++ distributions/configurations/iamscripts/recon/ADSearchQuery.groovy
@@ -4,7 +4,7 @@ if(role != null) {
     Set<RoleAttribute> attributes =  role.getRoleAttributes();
     for(RoleAttribute attr : attributes) {
         if("AD_OU".equals(attr.getName())) {
-            output="Get-ADUser -Filter {Name -like '*'} -SearchBase '"+attr.getValue()+"'";
+            output="Get-ADUser -Filter {Name -like '*'} -SearchBase '"+attr.getValue()+"' -Properties *";
             break;
         };
     }
