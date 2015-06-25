58cd9a7ea034182613ec66191ff08080fbf87d0b
Dmitry Zaporozhec <Dmitry.Zaporozhec@openiam.com>
2015-06-26 00:56:52 +0300

    remove non-mysql

--- conf/schema/oracle/3.2.2-RC2/Update_to_3.2.2-RC2.sql
+++
@@ -1,7 +0,0 @@
-/********************** v 3.2.2 - RC2***********************/
-/**********************ddl/2.extend_columns_length.sql*********************************************/
-ALTER TABLE USER_IDENTITY_ANS MODIFY (QUESTION_ANSWER VARCHAR(1024)  DEFAULT NULL);
-ALTER TABLE MANAGED_SYS MODIFY (PSWD VARCHAR(512)  DEFAULT NULL);
-
-commit;
-
